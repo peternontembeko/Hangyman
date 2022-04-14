0b5cdadd94a1e80fc2d6979e67ee3afc57d2f47f
peternontembeko <peternontembeko22@gmail.com>
2022-04-14 11:41:55 +0200

    added project files

---
+++ HangmanGameSln/Hangman.Core/Game/Hangman.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using HangmanRenderer.Renderer;
+
+namespace Hangman.Core.Game
+{
+    public class HangmanGame
+    {
+        private GallowsRenderer _renderer;
+
+        public HangmanGame()
+        {
+            _renderer = new GallowsRenderer();
+        }
+
+        public void Run()
+        {
+            _renderer.Render(5, 5, 6);
+
+            Console.SetCursorPosition(0, 13);
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write("Your current guess: ");
+            Console.WriteLine("--------------");
+            Console.SetCursorPosition(0, 15);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.Write("What is your next guess: ");
+            var nextGuess = Console.ReadLine();
+        }
+
+    }
+}
